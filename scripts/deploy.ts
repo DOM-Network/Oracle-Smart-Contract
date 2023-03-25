e30974dd39a4e13878d2ab09ce2b5b034d75b2ad
txthanh1178793 <truongxuanthanh1178793@gmail.com>
2023-03-26 02:58:10 +0700

    edit info

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,26 +11,28 @@ async function deployContractsFixture() {
   const blockNumBefore = await ethers.provider.getBlockNumber();
   const blockBefore = await ethers.provider.getBlock(blockNumBefore);
   const timestampBefore = blockBefore.timestamp;
+  console.log("Network: ", network);
+  console.log("Timestamp: ", blockBefore.timestamp)
 
   const [owner] = await ethers.getSigners();
   console.log('deploy from address: ', owner.address);
   const balance = await owner.getBalance();
-  console.log(balance);
+  // console.log(balance);
 
   const Admin = await ethers.getContractFactory("ProxyAdmin");
   const admin = await Admin.deploy();
   console.log('Admin address: ', admin.address);
-  await delay(10000);
+  await delay(5000);
 
   const PublisherRegistry = await ethers.getContractFactory("PublisherRegistry");
   const publisherRegistry = await PublisherRegistry.deploy();
   console.log('PublisherRegistry address: ', publisherRegistry.address);
-  await delay(10000);
+  await delay(5000);
 
   const Oracle = await ethers.getContractFactory("Oracle");
   const oracle = await Oracle.deploy();
   console.log('Oracle address: ', oracle.address);
-  await delay(10000);
+  await delay(5000);
 
   const fragment = Oracle.interface.getFunction("initialize");
   const data = Oracle.interface.encodeFunctionData(fragment, [
@@ -39,7 +41,7 @@ async function deployContractsFixture() {
     pairs
   ]);
 
-  await delay(10000);
+  await delay(5000);
   const TransparentUpgradeableProxy = await ethers.getContractFactory("TransparentUpgradeableProxy");
   const proxy = await TransparentUpgradeableProxy.deploy(oracle.address, admin.address, data);
   const oracleProxy = await Oracle.attach(proxy.address);
